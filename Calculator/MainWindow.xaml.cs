b1b1994ac0c7f4c96396ddb247121095d794f715
cjiahming <choong.jm@hotmail.com>
2023-08-22 23:40:54 +0800

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DoubleAnimation da = new DoubleAnimation(360, 0, new Duration(TimeSpan.FromSeconds(9)));
+            RotateTransform rt = new RotateTransform();
+            Rotate.RenderTransform = rt;
+            Rotate.RenderTransformOrigin = new Point(0.5, 0.5);
+            da.RepeatBehavior = RepeatBehavior.Forever;
+            rt.BeginAnimation(RotateTransform.AngleProperty, da);
+        }
+
+    
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "0";
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "1";
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "2";
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "3";
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "4";
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "5";
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "6";
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "7";
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "8";
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "9";
+        }
+
+        private void point_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + ".";
+        }
+
+        private void lPar_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "(";
+        }
+
+        private void rPar_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + ")";
+        }
+
+        private void plus_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "+";
+        }
+
+        private void minus_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "-";
+        }
+
+        private void mult_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "*";
+        }
+
+        private void div_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = screen.Text + "/";
+        }
+
+        private void result_Click(object sender, RoutedEventArgs e)
+        {
+            Type scriptType = Type.GetTypeFromCLSID(Guid.Parse("0E59F1D5-1FBE-11D0-8FF2-00A0D10038BC"));
+            dynamic obj = Activator.CreateInstance(scriptType, false);
+            obj.Language = "javascript";
+            string str = null;
+
+            try
+            {
+                var res = obj.Eval(screen.Text);
+                str = Convert.ToString(res);
+                screen.Text = screen.Text + "=" + str;
+            }
+            catch(SystemException)
+            {
+                screen.Text = "Syntax Error";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            screen.Text = "";
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            if(screen.Text != "")
+            {
+                screen.Text = screen.Text.Remove(screen.Text.Length - 1);
+            }
+        }
+    }
+}
